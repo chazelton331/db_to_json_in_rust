0e2f96fceedb73198625f13389a5a05d8799b9db
Cliff H <cliffhazelton@gmail.com>
2016-09-21 20:53:04 -0400

    Simplifed the first example code in main.rs to compile.

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ Compiled with
 ```
 rustc 1.12.0-beta.2 (389dad798 2016-08-24)
 ```
+
+##### TODO
+
+1. Add `signed_up_at` timestamp to the `User` struct, and add it to the response.
+
+```
+signed_up_at:   chrono::DateTime<UTC>, // ??
+```
+
+2. Add `Option` for values that can be `null` and add an example to `seeds.sql`
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 extern crate postgres;
-extern crate chrono;
 
 use postgres::{Connection, SslMode};
-//use postgres::types::Timestamp;
-
-use chrono::offset::utc::*;
-//use self::chrono::{TimeZone, NaiveDate, NaiveTime, NaiveDateTime, DateTime, UTC};
 
 struct User {
     id:             i32,
@@ -13,33 +8,32 @@ struct User {
     first_name:     String,
     last_name:      String,
     age:            i32,
-    signed_up_at:   chrono::DateTime<UTC>,
     referred:       bool,
     bio:            String,
 }
 
 fn main() {
-    let user = "cliff";
-    let pass = "";
-    let host = "localhost";
-    let port = 5432;
-    let db   = "testing_rust";
+    //let user = "cliff";
+    //let pass = "";
+    //let host = "localhost";
+    //let port = 5432;
+    //let db   = "testing_rust";
+    //let conn_str = format!("postgres://{}:{}@{}:{}/{}", user, pass, host, port, db);
+    //let () = conn_str;
+    //let conn     = Connection::connect(conn_str.to_string(), SslMode::None).unwrap();
+    let conn = Connection::connect("postgres://cliff:@localhost:5432/testing_rust", SslMode::None).unwrap();
 
-    let mut conn_str = format!("postgres://{}:{}@{}:{}/{}", user, pass, host, port, db);
-    let     conn     = Connection::connect(conn_str, SslMode::None).unwrap();
-
-    for row in &conn.query("select id, email, first_name, last_name, age, signed_up_at, referred, bio from users", &[]).unwrap() {
+    for row in &conn.query("select id, email, first_name, last_name, age, referred, bio from users", &[]).unwrap() {
         let user = User {
             id:             row.get(0), //fields in a row can be accessed either by their indices or their column names ... access by index is more efficient
             email:          row.get(1),
             first_name:     row.get(2),
             last_name:      row.get(3),
             age:            row.get(4),
-            signed_up_at:   row.get(5),
-            referred:       row.get(6),
-            bio:            row.get(7),
+            referred:       row.get(5),
+            bio:            row.get(6),
         };
 
-        println!("found user id={} email={} fn={} ln={} age={} signed_up_at={} ref={} bio={}", user.id, user.email, user.first_name, user.last_name, user.age, user.signed_up_at, user.referred, user.bio);
+        println!("found user id={} email={} fn={} ln={} age={} ref={} bio={}\n", user.id, user.email, user.first_name, user.last_name, user.age, user.referred, user.bio);
     }
 }
